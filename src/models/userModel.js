6573bf60e6f85a2186c428f8ae44d9727e2090f8
Manudies <manudies@gmail.com>
2024-05-20 16:51:07 +0200

    feat: creo el modelo de ususario

---
+++ src/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    email :{
+        type: String,
+        require:true,
+        unique: true,
+    },
+    username:{
+        type: String,
+        require: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        require: true,
+        
+    }, 
+    role: {
+        type:String,
+        enum: ["user","jardinero", "admin"],
+        default: "user"
+    }
+})
+
+const userModel = mongoose.model ("user", userSchema);
+
+export default userModel;
