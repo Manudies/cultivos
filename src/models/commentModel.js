a4a236f5dfa62e6bb5990bf9baa1524c846809c4
Manudies <manudies@gmail.com>
2024-05-21 13:44:53 +0200

    fix: modifico el modelo de comentarios y cultivos

--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
+import cultivationApiController from "../controllers/cultivations/cultivationApiController";
 
 const commentSchema = new mongoose.Schema({
-    user: [
-        {
+    user:{
             type: mongoose.Schema.Types.ObjectId,
             ref: "users"
         },
-    ],
     post: {
         type: String,
         require: true
     },
+    cultivation: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "cultivations"
+    }
 })  
 
 
